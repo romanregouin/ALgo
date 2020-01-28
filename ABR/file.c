a2f7ff5e1319df5d3f3b373d5cc18830119b1487
Andrieux <andrieuxliam@gmail.com>
2020-01-28 14:29:59 +0100

    file

--- ABR/file.c
+++ ABR/file.c
@@ -5,18 +5,15 @@
 
 pfile_t creer_file ()
 {
-  /* 
-     a completer
-   */
-  return NULL ;
+  pfile_t f=malloc(sizeof(pfile_t));
+  f->tete=-1;
+  f->queue=0;
 }
 
 int detruire_file (pfile_t f)
 {
-  /* 
-     a completer
-   */
-  return 0 ;
+  free(f);
+  return 0;
 }
 
 
@@ -24,32 +21,32 @@ int detruire_file (pfile_t f)
 
 int file_vide (pfile_t f)
 {
-  /*
-    a completer
-  */
-  return 0 ;
+  if(f->tete==-1)return 1;
+  return 0;
 }
 
 int file_pleine (pfile_t f)
   {
-  /*
-    a completer
-  */
-  return 0 ;
+  if(f->queue==f->tete)return 1;
+  return 0;
 }
 
 pnoeud_t defiler (pfile_t f)
   {
-  /*
-    a completer
-  */
-  return NULL ;
+  if(!file_vide(f)){
+    pnoeud_t tmp=f->Tab[f->tete];
+    f->tete=(f->tete+1)%MAX_FILE_SIZE;
+    if(f->tete==f->queue)f->tete=-1;
+    return tmp;
+  }else return NULL;
 }
 
 int enfiler (pfile_t f, pnoeud_t p)
 {
-  /*
-    a completer
-  */
-  return 0 ;
+  if(!file_pleine(f)){
+    f->Tab[f->queue]=p;
+    if(f->tete==-1)f->tete=f->queue;
+    f->queue=(f->queue+1)%MAX_FILE_SIZE;
+    return 0;
+  }else return 1;
 }
