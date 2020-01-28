f1a887c7e37cf27936e62b5700367b46f3277f80
Roman <romanregouin@live.fr>
2020-01-28 14:17:49 +0100

    Pile Completed not tested

--- ABR/pile.c
+++ ABR/pile.c
@@ -4,54 +4,44 @@
 
 ppile_t creer_pile ()
 {
-  /*
-    a completer
-  */
-  return NULL ;
+  ppile_t p;
+  p = malloc(sizeof(ppile_t));
+  if(p==NULL){
+    printf("Erreur allocation\n");
+    exit(-1);
+  }
+  p->sommet = 0;
+  return p ;
 }
 
 int detruire_pile (ppile_t p)
 {
-  /*
-    a completer
-  */
-  return 0 ;
+  free(p);
+  return 1;
 }  
 
 int pile_vide (ppile_t p)
 {
-  /*
-     a completer
-  */
-
-  return 0 ;
+  return(p->sommet==0);
 }
 
 int pile_pleine (ppile_t p)
- {
-  /*
-     a completer
-  */
-
-  return 0 ;
+{
+  return(p->sommet==MAX_PILE_SIZE);
 } 
 
 pnoeud_t depiler (ppile_t p)
 {
-  /*
-     a completer
-  */
-
-  return NULL ;
-
+  p->sommet--;
+  return p->Tab[p->sommet];
 }
 
 int empiler (ppile_t p, pnoeud_t pn)
-  {
-  /*
-     a completer
-  */
-
-  return 0 ;
-
+{
+  if(!pile_pleine){
+    p->Tab[p->sommet] = pn;
+    p->sommet++;
+    return 1;
+  }
+  return -1; 
 }
